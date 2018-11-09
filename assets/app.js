e0c79a2883f5aaebb78bf4620f3df7d247476b1c
Emily Dalton <emilydalton@dhcp-10-105-205-249.wireless.northwestern.private>
2018-11-09 14:50:13 -0600

    css changes, adding google fonts, tried to clear input box after on click in JS

--- assets/app.js
+++ assets/app.js
@@ -51,7 +51,7 @@ function renderButtons() {
     // (this is necessary otherwise we will have repeat buttons)
     $("#buttons-view").empty();
   
-    // Looping through the array of movies
+    // Looping through the array of cities
     for (var i = 0; i < cities.length; i++) {
   
       // Then dynamicaly generating buttons for each city in the array
@@ -70,6 +70,7 @@ function renderButtons() {
   
   // // This function handles events where one button is clicked
   $("#add-city").on("click", function(event) {
+    
     event.preventDefault();
   
   //   // This line grabs the input from the textbox
@@ -77,30 +78,16 @@ function renderButtons() {
   
   //   // The city from the textbox is then added to our array
     cities.push(city);
-  
+    // $("#city-input").reset();
+
    // Calling renderButtons which handles the processing of our movie array
+   
+
   renderButtons();
    });
 
-  // function animate(){
+ $(document).on("click", ".city", getCity);
 
-  //     var state = $(this).attr("data-state");
-  //     if (state === "still") {
-  //       $(this).attr("src", $(this).attr("data-animate"));
-  //       $(this).attr("data-state", "animate");
-  //     } else {
-  //       $(this).attr("src", $(this).attr("data-still"));
-  //       $(this).attr("data-state", "still");
-  //     }
-     
-  //   }
-  
-  // // Function for displaying the movie info
-  // // We're adding a click event listener to all elements with the class "movie"
-  // // We're adding the event listener to the document itself because it will
-  // // work for dynamically generated elements
-  // // $(".movies").on("click") will only add listeners to elements that are on the page at that time
-$(document).on("click", ".city", getCity);
  
 $(document).on("click", ".gif", function animate(){
 
